69f4eedc8164221cd23ce824e989496000128ebc
Alex Pinto <apinto0704@gmail.com>
2020-04-29 15:05:56 -0700

    setting up auth

--- covidTracker.py
+++ covidTracker.py
@@ -2,6 +2,7 @@ import json
 import csv
 import requests
 from pymongo import MongoClient
+import pprint
 
 def main():
     stateDataURL = 'https://covidtracking.com/api/v1/states/daily.json'
@@ -9,15 +10,30 @@ def main():
     credsFile = 'credentials.json'
     configFile = 'trackerConfig.json'
     updateFiles(stateDataURL, countyDataURL)
-    authenticate(credsFile)
+    username, password, server, authDB, workDB = authenticate(credsFile)
     config = configure(configFile)
+
+    client = MongoClient(server,
+                    username=username,
+                    password=password,
+                    authSource=authDB,
+                    authMechanism='SCRAM-SHA-256')
+    db = client[workDB]
+    covidColl = db['covid']
+    pprint.pprint(covidColl.find_one())
    
 #authenticate MongoDB using credentials in credentials.json
 def authenticate(credentialFile):
+    username = ""
+    password = ""
     with open(credentialFile) as f:
         jsonData = json.loads("\n".join(f.readlines()))
         username = jsonData['username']
         password = jsonData['password']
+        server = jsonData['server']
+        authDB = jsonData['authDB']
+        workDB = jsonData['db']
+    return username, password, server, authDB, workDB
     
 #get the task configuration from trackConfig.json
 def configure(configFile):
