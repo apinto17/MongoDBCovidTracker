602d6e1162ff8d0a2690f0bd18f83dd9f27d0909
charles <cscott20>
2020-04-29 17:46:11 -0700

    updates DB instead of local files now

--- covidTracker.py
+++ covidTracker.py
@@ -5,25 +5,42 @@ from pymongo import MongoClient
 import pprint
 
 def main():
-    stateDataURL = 'https://covidtracking.com/api/v1/states/daily.json'
-    countyDataURL = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+    covidDataURL = 'https://covidtracking.com/api/v1/states/daily.json'
+    statesDataURL = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
     credsFile = 'credentials.json'
     configFile = 'trackerConfig.json'
-    updateFiles(stateDataURL, countyDataURL)
-    username, password, server, authDB, workDB = authenticate(credsFile)
+    db = getDB(credsFile)
+    updateDB(db,covidDataURL, statesDataURL)
     config = configure(configFile)
-
-    client = MongoClient(server,
-                    username=username,
-                    password=password,
-                    authSource=authDB,
-                    authMechanism='SCRAM-SHA-256')
-    db = client[workDB]
     covidColl = db['covid']
     pprint.pprint(covidColl.find_one())
-   
-#authenticate MongoDB using credentials in credentials.json
-def authenticate(credentialFile):
+
+def updateDB(db, covidDataURL, statesDataURL):
+    covidStr = requests.get(covidDataURL).text
+    statesCSV = requests.get(statesDataURL).text
+    covid = json.loads(covidStr)
+    states = csv2json(statesCSV)
+    db.covid.drop()
+    db.states.drop()
+    db.covid.insert_many(covid)
+    db.states.insert_many(states)
+
+def csv2json(csv):
+    newLis = []
+    header = csv.split("\n")[0].split(",")
+    for line in csv.split("\n"):
+        line = line.split(",")
+        if line == header:
+            pass
+        else:
+            dic = {}
+            for item in range(len(header)):
+                dic[header[item]] = line[item]
+            newLis.append(dic)  
+    return(newLis)
+
+#authenticate MongoDB using credentials in credentials.json and return Client
+def getDB(credentialFile):
     username = ""
     password = ""
     with open(credentialFile) as f:
@@ -33,8 +50,14 @@ def authenticate(credentialFile):
         server = jsonData['server']
         authDB = jsonData['authDB']
         workDB = jsonData['db']
-    return username, password, server, authDB, workDB
-    
+    client = MongoClient(server,
+                    username=username,
+                    password=password,
+                    authSource=authDB,
+                    authMechanism='SCRAM-SHA-256')
+    db = client[workDB]
+    return db  
+ 
 #get the task configuration from trackConfig.json
 def configure(configFile):
     with open(configFile) as f:
